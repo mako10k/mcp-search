a9b38ecb9212a2ce270723cbfa2a88cc7fe1e518
RefImage Development Team <dev@refimage.example.com>
2025-08-18 01:47:02 +0000

    chore(lint): add ESLint/Prettier/jscpd/ts-prune setup; update scripts; drop winston dep

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  env: { node: true, es2021: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: './tsconfig.json', sourceType: 'module' },
+  plugins: ['@typescript-eslint', 'prettier', 'sonarjs'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:prettier/recommended'
+  ],
+  ignorePatterns: ['dist', 'node_modules'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    'sonarjs/cognitive-complexity': ['warn', 20]
+  }
+};
